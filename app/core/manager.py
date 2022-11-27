e172d2d0d2bfe782d79404ff3345d38261155b9a
usmanCodes-alt <usman.ghani4501@gmail.com>
2022-11-27 16:21:03 +0500

    Created custom user model and wrote test for its creation and listing from the admin

---
+++ app/core/manager.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create, save and return the new user
+        """
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(
+            email=self.normalize_email(email),
+            **extra_fields
+        )
+        user.set_password(password)     # creates hash of the password
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Creates a super user
+        """
+        user = self.create_user(email=email, password=password)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.is_active = True
+
+        user.save(using=self._db)
+
+        return user
--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,20 @@
-from django.db import models  # noqa
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin
+)
+from core.manager import UserManager
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        max_length=255,
+        unique=True
+    )
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
